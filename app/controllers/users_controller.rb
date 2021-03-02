0a57509df77ecb38a37929c9e674b76ba45ac94a
Kenechukwu Oguagbaka <kc101com@gmail.com>
2021-03-02 11:48:44 +0100

    Add user/new form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,23 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
+  end
+  
+    def create
+    @user = User.new(user_params)
+  if @user.save
+    redirect_to new_user_path
+  else
+    render :new
+  end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name)
   end
 end
