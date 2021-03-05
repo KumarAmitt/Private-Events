af9c12444cd7a62507124e7d9a248113739f4856
KumarAmitt <amitt2015@outlook.com>
2021-03-05 14:38:47 +0530

    Add Upcoming and previous events in user show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,20 +8,20 @@ class UsersController < ApplicationController
     @upcoming_events = @user.attended_events.where("event_date >= ?", Time.zone.now)
     @prev_events = @user.attended_events.where("event_date < ?", Time.zone.now)
   end
-  
-    def create
+
+  def create
     @user = User.new(user_params)
     session[:user_id] = @user.id
-  if @user.save
-    redirect_to new_session_path
-  else
-    render :new
-  end
-  end
+    if @user.save
+      redirect_to new_session_path
+    else
+      render :new
+    end
+end
 
   private
 
-    def user_params
-      params.require(:user).permit(:name)
-  end
+  def user_params
+    params.require(:user).permit(:name)
+end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :events, foreign_key: :creator_id
   has_many :event_attendees, foreign_key: :attendee_id
-  has_many :attended_events, through: :event_attendees, source: :attended_event 
+  has_many :attended_events, through: :event_attendees, source: :attended_event
+
+  # scope :upcoming, -> { where('event_date >= ?', Time.zone.now) }
+  # scope :previous, -> { where('event_date < ?', Time.zone.now) }
 end
